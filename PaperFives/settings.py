da8b57827e1e5186310b35242950f8c5e90537ce
Turmoil <tony-turmoil@qq.com>
2023-05-14 23:27:32 +0800

    update: more precise settings

--- PaperFives/settings.py
+++ PaperFives/settings.py
@@ -9,25 +9,35 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
+import yaml
 
+################################################################################
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+################################################################################
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-xx5bu8t(gfq96b0*cpu+5&9%^ndw)alhb4wd(h6g3-z)*sdi-e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# Load secrets
+if DEBUG:
+    with open("secrets_debug.yaml", 'r') as f:
+        SECRETS = yaml.safe_load(f)
+else:
+    with open("secrets.yaml", 'r') as f:
+        SECRETS = yaml.safe_load(f)
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = SECRETS['key']
 
+ALLOWED_HOSTS = ['*']
+
+################################################################################
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -47,6 +58,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "PaperFives.urls"
@@ -54,8 +66,7 @@ ROOT_URLCONF = "PaperFives.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
-        ,
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -70,18 +81,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "PaperFives.wsgi.application"
 
-
+################################################################################
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': SECRETS['database']
 }
 
-
+################################################################################
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -100,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+################################################################################
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -112,12 +120,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+################################################################################
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
 
+################################################################################
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
