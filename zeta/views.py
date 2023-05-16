2bfbbb6aab0bf8c81d8fa4df477ad3c88a818831
Turmoil <tony-turmoil@qq.com>
2023-05-16 21:56:22 +0800

    update: add sample page

--- zeta/views.py
+++ zeta/views.py
@@ -1,3 +1,8 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+def sample(request):
+    if request.method == "POST":
+        return JsonResponse({"Invalid request"})
+    return render(request, 'welcome.html')
