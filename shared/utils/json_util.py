bd27518dfa51273534aef3468daf4d8295345591
Turmoil <tony-turmoil@qq.com>
2023-05-16 22:50:27 +0800

    feat: add str and json utils

---
+++ shared/utils/json_util.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2023 - 2023 Tony Skywalker. All Rights Reserved 
+#
+# @Time    : 5/16/2023 22:18
+# @Author  : Tony Skywalker
+# @File    : json_util.py
+#
+# Description:
+#   This util file is a replacement of native support of JSON
+# converter in C#. It can be used to convert object to JSON string,
+# or a JSON string to a Python object.
+#   These functions has passed basic tests, and is guaranteed to
+# support datetime type, which is used in Django models. :)
+#
+
+import datetime
+import json
+from json import JSONEncoder
+
+
+class AdvancedEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.strftime("%Y-%m-%d %H:%M:%S")
+        return obj.__dict__
+
+
+def serialize(obj) -> str:
+    return json.dumps(obj, cls=AdvancedEncoder)
+
+
+def deserialize(json_str: str):
+    return json.loads(json_str)
---
+++ shared/utils/str_util.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2023 - 2023 Tony Skywalker. All Rights Reserved 
+#
+# @Time    : 5/16/2023 22:13
+# @Author  : Tony Skywalker
+# @File    : str_util.py
+#
+# Description:
+#   This util file is as a replacement for some useful native string
+# support in C#.
+#
+
+def is_null_or_empty(s: str) -> bool:
+    if s is None:
+        return True
+    if len(s) == 0:
+        return True
+    return False
+
+
+def is_no_content(s: str) -> bool:
+    if is_null_or_empty(s):
+        return True
+    return s.isspace()
+
+
+def make_not_null(s: str) -> bool:
+    if is_null_or_empty(s):
+        s = ""
+        return True
+    return False
