de430570620a91f0e817446bb7cd6e27ecc137f1
Turmoil <tony-turmoil@qq.com>
2023-05-18 09:40:26 +0800

    feat: add admin page

--- msgs/admin.py
+++ msgs/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from users.models import *
+from papers.models import *
+from msgs.models import *
+
+admin.site.register(UserAttribute)
+admin.site.register(UserStatistics)
+admin.site.register(User)
+admin.site.register(FavoriteUser)
+
+admin.site.register(PaperAttribute)
+admin.site.register(PaperStatistics)
+admin.site.register(Paper)
+admin.site.register(Author)
+admin.site.register(Reference)
+admin.site.register(FavoritePaper)
+
+admin.site.register(Message)
+admin.site.register(TextPayload)
+admin.site.register(LinkPayload)
+admin.site.register(ImagePayload)
+
