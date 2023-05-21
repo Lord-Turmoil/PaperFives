7f2f9dd6875bb3b617b0775739d0e85d324e0786
BUAAkang <932563835@qq.com>
2023-05-21 18:20:50 +0800

    feat: add Dto file about paper subsystem

--- shared/dtos/models/papers.py
+++ shared/dtos/models/papers.py
@@ -3,3 +3,23 @@
 # @Time    : 2023/5/21 14:31
 # @Author  : Lynx
 # @File    : papers.py
+
+from shared.dtos.models.base import BaseDto
+
+class PaperAttrDto(BaseDto):
+    def __init__(self):
+        self.title: str = ""
+        self.keywords: str = ""
+        self.abstract: str = ""
+        self.publish_date: str = "" # is that appropriate?
+
+class PaperDto:
+    def __init__(self):
+        self.pid: int = 0 # is that appropriate?
+        self.path: str = ""
+
+class AuthorDto:
+    def __init__(self):
+        self.email: str = ""
+        self.name: str = ""
+        self.order: int = 0
