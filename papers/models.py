f4105839dc0a87e8cb5c49e183a30d5b7eb8e4f4
BUAAkang <932563835@qq.com>
2023-05-21 21:33:44 +0800

    update: create model about paper's area

--- papers/models.py
+++ papers/models.py
@@ -71,7 +71,24 @@ class Paper(models.Model):
         ordering = ['pid']
         verbose_name = 'paper'
 
+class Area(models.Model):
+    first_level_discipline_code = models.IntegerField()
+    second_level_discipline_code = models.IntegerField()
+    name = models.CharField(max_length=63)
 
+    @classmethod
+    def create(cls, _FLDC, _SLDC, _name):
+        return cls(first_level_discipline_code = _FLDC, second_level_discipline_code = _SLDC, name = _name)
+
+    class Meta:
+        verbose_name = "area"
+
+class PaperAreaRelation(models.Model):
+    paper = models.ForeignKey(Paper, related_name="areas", on_delete=models.CASCADE)
+    area = models.ForeignKey(Area, related_name="papers", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "paper_area_relation"
 class Author(models.Model):
     email = models.EmailField()  # will be linked to User via email
     name = models.CharField(max_length=63)
