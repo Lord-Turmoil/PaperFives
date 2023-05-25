532fb673497d0496df224342caf7997017ee3fcf
Turmoil <tony-turmoil@qq.com>
2023-05-25 08:47:46 +0800

    fix: timezone problem

--- PaperFives/settings.py
+++ PaperFives/settings.py
@@ -127,8 +127,11 @@ LANGUAGE_CODE = "en-us"
 TIME_ZONE = "Asia/Shanghai"
 
 USE_I18N = True
+USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
+
+
 
 ################################################################################
 # Static files (CSS, JavaScript, Images)
