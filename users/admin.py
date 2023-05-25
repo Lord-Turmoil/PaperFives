21eed8f22c6d4b1f9739e9ef19934cfe1d5a7eea
Turmoil <tony-turmoil@qq.com>
2023-05-25 12:33:27 +0800

    update: admin models

--- users/admin.py
+++ users/admin.py
@@ -1,1 +1,10 @@
+from django.contrib import admin
+
+from users.models import *
+
 # Register your models here.
+admin.site.register(UserAttribute)
+admin.site.register(UserStatistics)
+admin.site.register(User)
+admin.site.register(Role)
+admin.site.register(FavoriteUser)
